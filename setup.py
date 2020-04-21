696e521b8cd30a274c1cedfd96236f8558f800ec
Christian Jauregui <cjauregui@econ.berkeley.edu>
2020-04-20 23:01:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+with open(path.join(here, 'README.md'), "r") as readme:
+    long_description = readme.read()
 
 NAME = 'famafrench'
 
