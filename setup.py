08570e93430507edd5938a2816948b1a907e291f
Christian Jauregui <cjauregui@econ.berkeley.edu>
2020-04-25 19:48:58 -0400

    Updated .py files

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-"""This file is part of famafrench.
+"""
+This file is part of famafrench.
 Copyright (c) 2020, Christian Jauregui <chris.jauregui@berkeley.edu>
 See file LICENSE.txt for license information.
 
