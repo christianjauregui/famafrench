9048575410898a9fb3a8f3287421552489280c61
Christian Jauregui <cjauregui@econ.berkeley.edu>
2020-05-01 14:40:15 -0400

    Update version.py

--- famafrench/version.py
+++ famafrench/version.py
@@ -6,8 +6,7 @@ See file LICENSE.txt for license information.
 This file must be kept very simple, because it is consumed from several places.
 It is imported by famafrench/__init__.py, execfile'd by setup.py, etc.
 
-I use a simple scheme:
-   1.0.0 -> 1.0.0+dev -> 1.1.0 -> 1.1.0+dev
+I use a simple scheme: 1.0.0 -> 1.0.0+dev -> 1.1.0 -> 1.1.0+dev
 where the +dev versions are never publicly released - they are just what I
 stick into the VCS in between releases.
 
