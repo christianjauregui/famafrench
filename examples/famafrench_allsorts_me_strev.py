da539e0df888f7d002d4cd5b40680b0c0e573d72
Christian Jauregui <cjauregui@econ.berkeley.edu>
2020-05-01 14:41:46 -0400

    Update famafrench_allsorts_me_strev.py

--- examples/famafrench_allsorts_me_strev.py
+++ examples/famafrench_allsorts_me_strev.py
@@ -32,7 +32,7 @@ https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/Data_Library/det_25_port
 import famafrench.famafrench as ff
 import datetime as dt
 
-startDate = dt.date(1950, 1, 1)  # "default" startDate
+startDate = dt.date(1960, 1, 1)  # "default" startDate
 endDate = dt.date.today()  # "default" endDate
 #startDate = dt.date(1970, 1, 1)
 #endDate = dt.date(2019, 12, 31)
