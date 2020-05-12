27fa69c228806d9bd291cb98f219cbff18236fac
Christian Jauregui <cjauregui@econ.berkeley.edu>
2020-05-11 20:49:31 -0400

    Update wrdsconnect.py

--- famafrench/wrdsconnect.py
+++ famafrench/wrdsconnect.py
@@ -11,7 +11,7 @@ Description
 ___________
 wrdsConnection
     Object class used to set up a remote connection to `wrds-cloud`.
-    This class largely builds on the "Connection" class in the `WRDS-Py` library.
+    This largely builds on the "Connection" class in the `WRDS-Py` library.
 """
 
 __author__ = 'Christian Jauregui <chris.jauregui@berkeley.edu'
